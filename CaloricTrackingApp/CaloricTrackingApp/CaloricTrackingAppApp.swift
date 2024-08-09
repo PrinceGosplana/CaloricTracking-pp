3363e9499719604f4ab78825fd8609183a07df0b
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-09 17:36:26 +0300

    init the project

---
+++ CaloricTrackingApp/CaloricTrackingApp/CaloricTrackingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloricTrackingAppApp.swift
+//  CaloricTrackingApp
+//
+//  Created by Oleksandr Isaiev on 09.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct CaloricTrackingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
